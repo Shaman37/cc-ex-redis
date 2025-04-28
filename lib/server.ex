9678b2612948cffeb3ccbbd43e62b6786adc066a
João Leal <github.onto605@passinbox.com>
2025-04-28 22:02:44 +0100

    jm1: bind to a port

--- lib/server.ex
+++ lib/server.ex
@@ -2,25 +2,17 @@ defmodule Server do
   @moduledoc """
   Your implementation of a Redis server
   """
+  @port 6379
 
   use Application
 
   def start(_type, _args) do
-    Supervisor.start_link([{Task, fn -> Server.listen() end}], strategy: :one_for_one)
+    Supervisor.start_link([{Task, fn -> listen() end}], strategy: :one_for_one)
   end
 
-  @doc """
-  Listen for incoming connections
-  """
-  def listen() do
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
-    IO.puts("Logs from your program will appear here!")
-
-    # Uncomment this block to pass the first stage
-    #
-    # # Since the tester restarts your program quite often, setting SO_REUSEADDR
-    # # ensures that we don't run into 'Address already in use' errors
-    # {:ok, socket} = :gen_tcp.listen(6379, [:binary, active: false, reuseaddr: true])
-    # {:ok, _client} = :gen_tcp.accept(socket)
+  # Listen for incoming connections
+  defp listen() do
+    {:ok, socket} = :gen_tcp.listen(@port, [:binary, active: false, reuseaddr: true])
+    {:ok, _client} = :gen_tcp.accept(socket)
   end
 end

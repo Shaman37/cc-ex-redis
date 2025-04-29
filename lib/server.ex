63644e4c7a2126e93fc3f8307aee23eb1361d33f
João Leal <github.onto605@passinbox.com>
2025-04-29 20:41:18 +0100

    wy1: respond to multiple PINGs

--- lib/server.ex
+++ lib/server.ex
@@ -1,21 +1,40 @@
 defmodule Server do
   @moduledoc """
-  Your implementation of a Redis server
+  Implementation of a Redis server.
   """
+
   @port 6379
 
   use Application
 
+  @doc """
+  Starts the Redis server application and supervises the listener task.
+  """
+  @spec start(:normal | :ignore | {:error, term()}, list()) :: {:ok, pid()} | {:error, term()}
   def start(_type, _args) do
     Supervisor.start_link([{Task, fn -> listen() end}], strategy: :one_for_one)
   end
 
-  # Listen for incoming connections
+  # Opens a TCP socket on the configured port and accepts a single client connection.
+  @spec listen() :: :ok
   defp listen() do
     {:ok, socket} = :gen_tcp.listen(@port, [:binary, active: false, reuseaddr: true])
-    {:ok, client} = :gen_tcp.accept(socket)
+    {:ok, client_socket} = :gen_tcp.accept(socket)
 
-    :gen_tcp.send(client, "+PONG\r\n")
-    :gen_tcp.close(client)
+    loop_client_handle(client_socket)
+  end
+
+  # Continuously reads from the given client socket and responds with "+PONG\\r\\n".
+  # Terminates gracefully when the client disconnects.
+  @spec loop_client_handle(:gen_tcp.socket()) :: :ok
+  defp loop_client_handle(socket) do
+    case :gen_tcp.recv(socket, 0) do
+      {:ok, _data} ->
+        :gen_tcp.send(socket, "+PONG\r\n")
+        loop_client_handle(socket)
+
+      {:error, :closed} ->
+        :ok
+    end
   end
 end
